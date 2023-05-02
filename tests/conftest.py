da6166be27163ae8e84c273626ee79b21f4b0a1b
Беликов Владимир <belikrastr@yandex.ru>
2023-05-03 01:57:55 +0300

    fix_error

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,3 @@ from os.path import abspath, dirname, join
 root_dir = dirname(dirname(abspath(__file__)))
 sys.path.append(root_dir)
 infra_dir_path = join(root_dir, 'infra')
-
-pytest_plugins = [
-]
