bb198284af0b0591837b0ecc1000e6ee04985556
Беликов Владимир <belikrastr@yandex.ru>
2023-05-03 23:44:41 +0300

    git

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,6 @@ from os.path import abspath, dirname, join
 root_dir = dirname(dirname(abspath(__file__)))
 sys.path.append(root_dir)
 infra_dir_path = join(root_dir, 'infra')
+
+pytest_plugins = [
+]
